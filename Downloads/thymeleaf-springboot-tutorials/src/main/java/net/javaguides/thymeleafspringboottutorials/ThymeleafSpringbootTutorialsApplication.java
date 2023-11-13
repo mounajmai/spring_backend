944fae995ab73acc3f3b3499c6a12563570bc803
mouna jmai <mouna.jmai@gmail.com>
2023-11-13 01:28:58 +0100

    commit tp

---
+++ Downloads/thymeleaf-springboot-tutorials/src/main/java/net/javaguides/thymeleafspringboottutorials/ThymeleafSpringbootTutorialsApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.thymeleafspringboottutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSpringbootTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafSpringbootTutorialsApplication.class, args);
+	}
+
+}
